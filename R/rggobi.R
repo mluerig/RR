324e8b0f73e1651f931ead981306e4e99f7b5263
mluerig <moritz.luerig@gmail.com>
2018-02-07 14:15:44 +0100

    final update

--- R/rggobi.R
+++ R/rggobi.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(rggobi)
 library(cowplot) # makes nice bw plots
 
+
 setwd("E://GitHub//RR//R//")
 
 
@@ -29,13 +30,22 @@ g <- ggobi(iso)
 
 # go the interaction > identify. you can change the value you would like to have returned, e.g. Family or ID
 
-# go to tools > automatic brush and select family from the dropdown
+# go to tools > automatic brush and select family from the dropdown ... 
+# ... or do it from R:
+
+
+glyph_colour(g[1])<-iso$Family
+
+
 
 # make up fake data to do a PCA:
-iso$Width<-iso$log.Length*
-iso$HeadSize
+iso$Width <- sqrt(iso$Area)*runif(nrow(iso), min=.5, max=1.8) 
+iso$HeadArea <- (iso$Area/6)*runif(nrow(iso), min=0.4, max=1.6)
 
+g <- ggobi(iso)
 
-# lets pretend "Area" and 
+# go to tools > sphering  and add variables you want include in a PCA (shift hold + click)
+# save file, and read to R
 
+iso.px<-read.csv("iso.pca.csv", header=T)
 
